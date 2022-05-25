e1288a6d66054288ed65d0e640a46011fbcd5e4b
GopiKrishnan Ganesan <gopikrishnan.ganesan@gmail.com>
2022-05-25 20:50:30 +0530

    cleanup

--- src/test/java/org/people/weijuly/imfs/impl/SpringTestConfiguration.java
+++ src/test/java/org/people/weijuly/imfs/impl/SpringTestConfiguration.java
@@ -4,6 +4,5 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
-@ContextConfiguration(classes = {TestConfiguration.class})
 public class SpringTestConfiguration {
 }
